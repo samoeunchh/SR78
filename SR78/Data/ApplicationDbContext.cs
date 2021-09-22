7e8f38334167b3a337f279b663a5fc28f324f40c
samoeun chhay <samoeunchh@gmail.com>
2021-09-22 09:32:26 +0700

    Add project files.

---
+++ SR78/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SR78.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SR78.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<VehicleType> VehicleType { get; set; }
+    }
+}
---
+++ SR78/Models/VehicleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR78.Models
+{
+    public class VehicleType
+    {
+        [Key]
+        public Guid VehicleTypeId { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage ="Vehicle Type Name field is required!")]
+        [Display(Name = "Vehicle Type Name")]
+        public string VehicleTypeName { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+    }
+}
